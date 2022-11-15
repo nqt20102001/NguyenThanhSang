2190f6646b6dfa580414a133233386debe3c9edc
nqt20102001 <ngquoctoan2001@gmail.com>
2022-11-15 17:11:44 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {tshitsList, hoodieList} from "./productList.js";
+import {tshitsList, hoodiesList, sweatersList, productsList} from "./productList.js";
 
 // ======== OPEN NAVBAR IN MOBILE =========
 const bar = document.getElementById('bar');
@@ -44,16 +44,18 @@ const btnCartMobile = document.querySelector("#btn-cart-mobile");
 
 const shop = document.querySelector("#shopp");
 const cart = document.querySelector("#cartt");
+const menuWrapper = document.querySelector("#menu-wrapper");
 
 btnShop.addEventListener('click', () => {
     shop.style.display = 'block';
     cart.style.display = 'none';
+    menuWrapper.style.display = 'block';
 })
 
 btnCart.addEventListener('click', () => {
     shop.style.display = 'none';
     cart.style.display = 'block';
-
+    menuWrapper.style.display = 'none';
 })
 
 btnCartMobile.addEventListener('click', () => {
@@ -151,7 +153,8 @@ const Orders = [
 
 
 // ======= OPEN MODAL (DETAILS PRODUCT) =======
-const htmls = tshitsList.map((product,index)=>{
+const htmls = productsList.map((product,index)=>{
+    
     return`
     <div>
         <input type="checkbox" name="product" id="p${index}"/>
@@ -226,3 +229,153 @@ smallImg[2].onclick = function(){
 smallImg[3].onclick = function(){
     mainImg.src = smallImg[3].src;
 }
+
+
+
+// ========= ADD PRODUCTS IN CART ==========
+const btnAddCarts = document.querySelectorAll("#btn-add-cart");
+
+btnAddCarts.forEach(function(button, index) {
+    button.addEventListener('click', function(event) {
+        var btnAddCart = event.target;
+        var product = btnAddCart.parentElement;
+        var productDetail = product.parentElement;
+        var productImg = productDetail.querySelector('#mainImg').src;
+        var productName = productDetail.querySelector('#name-product').innerText;
+        var productPrice = productDetail.querySelector('#price-product').innerText;
+
+        addCart(productImg, productName, productPrice);
+    })
+})
+
+function addCart(productImg, productName, productPrice) {
+    var addTr = document.createElement("tr");
+    var cartItems = document.querySelectorAll("#cart tbody tr");
+    
+    for(var i = 0 ; i < cartItems.length; i++) {
+        var productItem = document.querySelectorAll("#name-product");
+        if(productItem[i].innerHTML == productName) {
+            console.log(productName);
+            var quantityProduct = cartItems[i].querySelector("#cart tbody tr input").value;
+            var quantityProducts = Number(quantityProduct);
+            console.log(quantityProducts.toString());
+            // quantityProducts += 1;
+            // alert("Sản phẩm đã có trong giỏ hàng!");
+        }
+    }
+    var trContent = `
+        <td><button id="remove-product"><i class="fa-sharp fa-solid fa-circle-xmark"></i></button></td>
+        <td><img src="${productImg}" alt=""></td>
+        <td id="name-product" style="font-weight: 600;">${productName}</td>
+        <td id="price-product">${productPrice}</td>
+        <td><input type="number" value="1"></td>
+    `
+    addTr.innerHTML = trContent;
+    var cartTable = document.querySelector("tbody");
+    cartTable.append(addTr);
+    cartToTal();
+}
+
+
+// ======= TOTAL MONEY CART ========
+function cartToTal() {
+    var cartItems = document.querySelectorAll("#cart tbody tr");
+    var totalPrice = 0;
+
+    for(var i = 0 ; i < cartItems.length; i++){
+        var inputValue = cartItems[i].querySelector("#cart tbody tr input").value;
+        var productPrice = cartItems[i].querySelector("#price-product").innerText;
+        var total = (inputValue*productPrice)*1000;
+        totalPrice += total;
+    }
+
+    var subTotal = document.querySelector("#subtotal span");
+    subTotal.innerHTML = totalPrice.toLocaleString('de-DE');
+}
+
+
+// ======= DELETE PRODUCT IN CART ==========
+function deleteCart() {
+    for(var i = 0 ; i < cartItems.length; i++) {
+        
+    }
+}
+
+
+// =========== PAGINATION =========
+// function pages(current_page, last_page, onSides = 3) {
+//     // pages
+//     let pages = [];
+//     // Loop through
+//     for (let i = 1; i <= last_page; i++) {
+//         // Define offset
+//         let offset = (i == 1 || last_page) ? onSides + 1 : onSides;
+//         // If added
+//         if (i == 1 || (current_page - offset <= i && current_page + offset >= i) || 
+//             i == current_page || i == last_page) {
+//             pages.push(i);
+//         } else if (i == current_page - (offset + 1) || i == current_page + (offset + 1)) {
+//             pages.push('...');
+//         }
+//     }
+//     return pages;
+// // }
+
+
+// var productsList
+// if (localStorage.getItem('product') === null) {
+//     console.log("day du lieu len");
+//     productList = [
+//         { img: "img/non_1.png", id: "shirt", price: "100", name: "sp-1" },
+//         { img: "img/non_2.png", id: "shirt", price: "50", name: "sp-2" },
+//         { img: "img/non_3.png", id: "shirt", price: "100", name: "sp-3" },
+//         { img: "img/non_4.png", id: "shoes", price: "50", name: "sp-4" },
+//         { img: "img/non_5.png", id: "shoes", price: "100", name: "sp-5" },
+//         { img: "img/non_6.png", id: "shoes", price: "50", name: "sp-6" },
+//     ];
+//     localStorage.setItem('product', JSON.stringify(productList));
+// }
+// productList = JSON.parse(localStorage.getItem('product'))
+
+// let perPage = 6
+// let start = 0
+// let end = perPage
+
+// const btnNext = document.getElementById("next-btn");
+// const btnPrev = document.getElementById("prev-btn);
+// var currentPage = 1
+
+// function getCurrentPage(currentPage) {
+//     start = (currentPage - 1) * perPage
+//     end = currentPage * perPage
+// }
+// function inRa() {
+//     let listProduct = ""
+//     productList.map((item, index) => {
+
+//         if (index >= start && index < end) {
+//             listProduct += `
+//             <li class="element_product" id="`+ item.id + `">
+//                 <div class="mid-2--img both">
+//                     <div class="both-top" onclick="detailProduct(this)">
+//                         <span class="both-top--img"><img src="`+ item.img + `" alt=""></span>
+//                         <a class="both-top--muaNgay">SẢN PHẨM</a>
+//                     </div>
+//                     <div class="both-bot">
+//                         <div class="both-bot--name">
+//                             <h2 class="name_item">`+ item.name + `</h2>
+//                         </div>
+//                         <div class="price_item">`+ item.price + `</div>
+//                     </div>
+//                 </div>
+//                 <span id="detail"></span>
+//             </li>
+//             `
+//         }
+//         document.getElementById('pagination').innerHTML = listProduct
+//     })
+
+// }
+// totalPages = Math.ceil(productList.length / perPage);
+// renderListPage(totalPages)
+// inRa()
